ec2b2541354a05bdb6b3ee9b47ec091b3092be1f
basheera-sulthana <basheera.sulthana@gmail.com>
2023-01-05 11:39:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { useEffect } from "react";
+// import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  // useEffect(() => {
+  //   document.body.style.background =
+  //     "url('./bg4.jpg') no-repeat center center fixed";
+
+  //   document.body.style.backgroundSize = "cover";
+  // }, []);
+
+  return (
+    <>
+    <div>
+    <h1>App.js file</h1></div>
+     </>
+  );
+}
+
+export default App;
+ 
